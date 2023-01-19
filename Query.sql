b0c872d8fbc2d5bb34b5e5eb44467e32126c5423
slivcaigor <slivcaigor@gmail.com>
2023-01-19 19:56:48 +0100

    dopo le 14 fix

--- Query.sql
+++ Query.sql
@@ -16,7 +16,7 @@ SELECT * FROM courses WHERE period = 'I semestre' AND year = '1';
 
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 
-SELECT * FROM exams WHERE date = '2020-06-20' AND hour >= '14:00:00';
+SELECT * FROM exams WHERE date = '2020-06-20' AND hour > '14:00:00';
 
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
 
